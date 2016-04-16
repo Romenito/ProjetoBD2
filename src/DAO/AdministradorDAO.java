702c0ad286a3e2798862673808288b93cfd20ba5
Romenito <romen_damaceno@hotmail.com>
2016-04-16 18:49:29 -0300

    Mudança na Classe administrador

--- src/DAO/AdministradorDAO.java
+++ src/DAO/AdministradorDAO.java
@@ -5,10 +5,46 @@
  */
 package DAO;
 
+import Banco_Dados.JDBC_Conexao;
+import Modelo.Administrador;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Romen
  */
 public class AdministradorDAO {
+    private JDBC_Conexao conect;
     
+    
+    public AdministradorDAO(){
+        conect= new JDBC_Conexao();
+    }
+    
+     public void InserirAdministrador(Administrador administrador){
+        conect.conexao();
+        try {
+            conect.executaSQL("select * from revendedor");
+            conect.rs.last();
+            administrador.setId_revendedor(conect.rs.getInt("id_revendedor"));
+        } catch (SQLException ex) {
+            Logger.getLogger(AdministradorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            PreparedStatement pst= conect.conn.prepareStatement(" insert into administra(nome,telefone,email,id_revendedor) values(?,?,?,?)");
+             pst.setString(1,administrador.getNome());
+             pst.setString(2,administrador.getTelefone());
+             pst.setString(3,administrador.getEmail());
+             pst.setInt(4,administrador.getId_revendedor());
+             pst.execute();
+             conect.desconexao();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao cadastrar Adminstrador em Tabela"+ ex);
+        }
+        
+    }
 }
