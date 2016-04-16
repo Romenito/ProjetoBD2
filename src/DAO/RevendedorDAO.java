695f078bc5c60c187a0f5c46b792072cc8c5037d
Romenito <romen_damaceno@hotmail.com>
2016-04-16 18:48:31 -0300

    Mudança na Classe revendedor

--- src/DAO/RevendedorDAO.java
+++ src/DAO/RevendedorDAO.java
@@ -5,10 +5,40 @@
  */
 package DAO;
 
+import Banco_Dados.JDBC_Conexao;
+import Modelo.Revendedor;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Romen
  */
 public class RevendedorDAO {
+    private JDBC_Conexao conect;
     
+    
+    public RevendedorDAO(){
+        conect= new JDBC_Conexao();
+    }
+    
+    public void InserirRevendedor(Revendedor revendedor){
+        conect.conexao();
+        try {
+            PreparedStatement pst= conect.conn.prepareStatement("insert into revendedor(nome,sobrenome,cpf_revendedor,email) values(?,?,?,?)");
+             pst.setString(1,revendedor.getNome());
+             pst.setString(2,revendedor.getSobrenome());
+             pst.setString(3,revendedor.getCpf_revendedor());
+             pst.setString(4,revendedor.getEmail());
+             pst.execute();
+             conect.desconexao();
+             JOptionPane.showMessageDialog(null,"Revendedor cadastrado com Sucesso ");
+        } catch (SQLException ex) {
+            Logger.getLogger(RevendedorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
