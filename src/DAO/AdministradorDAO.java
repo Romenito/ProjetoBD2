bda27820293bc0fd03471a0eb2fe4d6fc083d32f
Romenito <romen_damaceno@hotmail.com>
2016-04-18 11:33:15 -0300

    Metodo de Pesquisar e Editar em ADM

--- src/DAO/AdministradorDAO.java
+++ src/DAO/AdministradorDAO.java
@@ -19,10 +19,12 @@ import javax.swing.JOptionPane;
  */
 public class AdministradorDAO {
     private JDBC_Conexao conect;
+    private Administrador pesqadm;
     
     
     public AdministradorDAO(){
         conect= new JDBC_Conexao();
+        pesqadm= new Administrador();
     }
     
      public void InserirAdministrador(Administrador administrador){
@@ -47,4 +49,44 @@ public class AdministradorDAO {
         }
         
     }
-}
+     public void EditarAdministrador(Administrador administrador){
+          conect.conexao();
+        
+        try {
+            conect.executaSQL("select * from administra");
+            conect.rs.last();
+            administrador.setId_revendedor(conect.rs.getInt("id_revendedor"));
+        } catch (SQLException ex) {
+            Logger.getLogger(RevendedorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            PreparedStatement  pst = conect.conn.prepareStatement("update administra set nome= ?,email= ?, telefone= ? where id_revendedor= ?");
+            pst.setString(1,administrador.getNome());
+            pst.setString(2,administrador.getEmail());
+            pst.setString(3,administrador.getTelefone());
+            pst.setInt(4,administrador.getId_revendedor());
+            pst.execute();
+            conect.desconexao();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao Editar Dados de Administrador"+ex);
+        }
+      }
+     public Administrador PesquisarAdministrador(){
+        conect.conexao();
+       
+        try {
+            conect.executaSQL("select * from administra");
+            conect.rs.last();
+            pesqadm.setNome(conect.rs.getString("nome"));
+            pesqadm.setEmail(conect.rs.getString("email"));
+            pesqadm.setTelefone(conect.rs.getString("telefone"));
+            conect.desconexao();
+        } catch (SQLException ex) {
+          
+        }
+        return pesqadm;
+    }
+   }
+    
+
